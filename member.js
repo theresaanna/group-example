b43af886b51893d188fc85a27c9e57c9cd5443e2
theresaanna <theresa@theresaanna.com>
2012-06-21 09:44:21 -0400

    basic output

--- member.js
+++ member.js
@@ -14,16 +14,19 @@ $(function() {
     var MemberList = Backbone.Collection.extend({
         model: Member,
 
-        localStorage: new Backbone.LocalStorage("group-members")
-    });
+        localStorage: new Backbone.LocalStorage("group-members"),
+
+        template: $('#member').html()
+   });
 
     var Members = new MemberList;
 
-    var MemberView = Backbone.View.extend({
+    var memberView = Backbone.View.extend({
+        collection: Members,
 
-        model: Member,
+        el: $('#members-list'),
 
-        el: $('.member-list'),
+
 
         events: {
             'click .remove': 'remove',
@@ -39,12 +42,20 @@ $(function() {
         },
 
         render: function() {
-            debugger;
+            this.collection.each(function(member) {
+                var name = member.attributes.name;
+                var rendered = Handlebars.compile(this.collection.template);
+                $(this.$el).append(rendered(member.attributes));
+            }, this);
+
+            return this;
         },
 
         initialize: function() {
-            Members.bind('all', this.render, this);
-
+            Members.bind('add', this.render, this);
+            Members.bind('reset', this.render, this);
+            Members.bind('remove', this.render, this);
+            
             if (Members.localStorage.records.length === 0) {
                 $.each(dummyData, function(i, value) {
                     var member = new Member({
@@ -58,10 +69,13 @@ $(function() {
                     Members.create(member);
                 });
             }
+            else {
+                Members.fetch();
+            }
         }
     });
 
-    var App = new MemberView;
+    var App = new memberView;
 });
 
 //global
@@ -69,62 +83,72 @@ dummyData = [{
         "name": "Person McPersonson",
         "id": "1",
         "photo": "images/avatar2.jpg",
-        "bio": "Cupcake ipsum dolor sit amet donut. Donut marshmallow topping caramels. Biscuit candy brownie gingerbread croissant I love."
+        "bio": "Cupcake ipsum dolor sit amet donut. Donut marshmallow topping caramels. Biscuit candy brownie gingerbread croissant I love.",
+        "date": "4/5/10"
     },
     {
         "name": "Spiffy McSpifferson",
         "id": "2",
         "photo": "images/avatar.png",
         "title": "Co-organizer",
-        "bio": "Jelly-o dessert biscuit sugar plum. Candy canes lollipop topping muffin soufflé cupcake. Pudding I love I love I love candy canes soufflé dragée."
+        "bio": "Jelly-o dessert biscuit sugar plum. Candy canes lollipop topping muffin soufflé cupcake. Pudding I love I love I love candy canes soufflé dragée.",
+        "date": "5/21/11"
     },
     {
         "name": "Mazement Superific",
         "id": "3",
         "photo": "images/avatar.png",
-        "bio": "Cupcake ipsum dolor sit amet donut. Donut marshmallow topping caramels. Biscuit candy brownie gingerbread croissant I love."
+        "bio": "Cupcake ipsum dolor sit amet donut. Donut marshmallow topping caramels. Biscuit candy brownie gingerbread croissant I love.",
+        "date": "5/21/11"
     },
     {
         "name": "Ebba Blomqvist",
         "id": "4",
         "photo": "images/avatar2.jpg",
-        "bio": "Jelly-o dessert biscuit sugar plum. Candy canes lollipop topping muffin soufflé cupcake. Pudding I love I love I love candy canes soufflé dragée."
+        "bio": "Jelly-o dessert biscuit sugar plum. Candy canes lollipop topping muffin soufflé cupcake. Pudding I love I love I love candy canes soufflé dragée.",
+        "date": "5/21/11"
     },
     {
         "name": "Melilot Sackville",
         "id": "5",
         "photo": "images/avatar.png",
         "title": "Organizer",
-        "bio": "Cake cheesecake candy canes topping gummies sweet roll."
+        "bio": "Cake cheesecake candy canes topping gummies sweet roll.",
+        "date": "5/21/11"
     },
     {
         "name": "Gemma Caird",
         "id": "6",
-        "photo": "images/avatar2.png",
-        "bio": "Marshmallow ice cream gummi bears cheesecake pudding dessert."
+        "photo": "images/avatar2.jpg",
+        "bio": "Marshmallow ice cream gummi bears cheesecake pudding dessert.",
+        "date": "5/21/11"
     },
     {
         "name": "Carolos Deslauriers",
         "id": "7",
-        "photo": "images/avatar.jpg",
-        "bio": "Wafer chocolate wafer."
+        "photo": "images/avatar.png",
+        "bio": "Wafer chocolate wafer.",
+        "date": "5/21/11"
     },
     {
         "name": "Damaris Zapata Arenas",
         "id": "8",
-        "photo": "images/avatar.jpg",
-        "bio": "Dessert pudding chocolate cake liquorice pudding brownie tiramisu."
+        "photo": "images/avatar.png",
+        "bio": "Dessert pudding chocolate cake liquorice pudding brownie tiramisu.",
+        "date": "5/21/11"
     },
     {
         "name": "Zukatokuchi",
         "id": "9",
-        "photo": "images/avatar2.png",
-        "bio": "Tart pastry chocolate bar bear claw chocolate apple pie toffee bonbon"
+        "photo": "images/avatar2.jpg",
+        "bio": "Tart pastry chocolate bar bear claw chocolate apple pie toffee bonbon",
+        "date": "5/21/11"
     },
     {
         "name": "Fazekas Zsuska",
         "id": "10",
         "photo": "images/avatar2.jpg",
-        "bio": "Applicake sugar plum powder gummi bears."
+        "bio": "Applicake sugar plum powder gummi bears.",
+        "date": "5/21/11"
     }
 ];
